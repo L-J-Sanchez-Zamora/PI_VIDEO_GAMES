46843db0d2dd3c6931c6570323d5a025be046cdc
Leidy Johanna Sánchez Zamora <124950194+L-J-Sanchez-Zamora@users.noreply.github.com>
2023-08-30 12:30:47 -0500

    Deploy

--- api/index.js
+++ api/index.js
@@ -18,11 +18,11 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-const PORT = 3005
+const port = process.env.PORT || 3005
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, ()=> console.log(`Listening on port: ${PORT}`)) // eslint-disable-line no-console
+  server.listen(port, ()=> console.log(`Listening on port: ${port}`)) // eslint-disable-line no-console
   });
 
